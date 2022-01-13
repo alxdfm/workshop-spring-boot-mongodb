1e6a29f9705bcd97771df99ecb61c1bf83bf9b35
Alexandre Machado <alexandre_fmachado@hotmail.com>
2022-01-12 23:34:32 -0300

    Criando entity Post com user aninhado

--- src/main/java/com/alexandre/workshopmongo/domain/User.java
+++ src/main/java/com/alexandre/workshopmongo/domain/User.java
@@ -8,13 +8,14 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
 public class User  implements Serializable{
-
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	private String id;
 	private String name;
 	private String email;
 	
-	public user() {	
+	public User() {	
 	}
 
 	public User(String id, String name, String email) {
